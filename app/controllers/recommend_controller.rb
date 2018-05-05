2f4a197eaa8c16d63d443b5f4abd8b229addd24d
Kevin Huang <kevinzhuang10@gmail.com>
2018-05-05 01:36:43 -0700

    add controller logic and yelp api integration

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -2,8 +2,39 @@ class RecommendController < ApplicationController
   include HTTParty
 
   def index
-    puts "hello >>>>>>>>>>>>>>>"
-    response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
-    render json: response
+    responses = []
+
+    yelp_URL = 'https://api.yelp.com/v3/businesses/search?'
+    query = ''
+    request_URL = ''
+    request_params = {
+      headers: {
+        Authorization: ENV['YELP_API_KEY']
+      }
+    }
+
+    categories = params[:term].split(' ')
+    categories.each do |category|
+      params[:term] = category
+      query_string = stringify_query(params)
+      request_URL = yelp_URL + query_string
+      responses.push(HTTParty.get(request_URL, request_params))
+    end
+
+    recommendations = responses.map do |response|
+      response.parsed_response["businesses"][0]
+    end
+
+    render json: recommendations
+  end
+
+  def stringify_query(query_object)
+    key_value_pairs = []
+    query_object.each do |k, v|
+      if k.to_s != 'controller' && k.to_s != 'action'
+        key_value_pairs.push("#{k.to_s}=#{v.to_s}")
+      end
+    end
+    key_value_pairs.join('&')
   end
 end
