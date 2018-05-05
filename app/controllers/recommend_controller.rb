b423737345583e88e598944dab8d6339cde4ae63
Kevin Huang <kevinzhuang10@gmail.com>
2018-05-04 23:57:28 -0700

    add dotenv gem

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -1,4 +1,9 @@
 class RecommendController < ApplicationController
+  include HTTParty
+
   def index
+    puts "hello >>>>>>>>>>>>>>>"
+    response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+    render json: response
   end
 end
